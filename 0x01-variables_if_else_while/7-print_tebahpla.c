abc576cadd209d293a701c23f849b3e364904ba0
zekariyas7 <zekariyas7721@gmail.com>
2022-12-10 05:48:17 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - prints is posetive, negative
+ *
+ * * or zero
+ *
+ * *Return: returns zero if program exits well.
+ *
+ *  */
+
+int main(void)
+
+{
+
+	char c = 'z';
+
+	while (c >= 'a')
+
+	{
+
+		putchar(c);
+
+		c--;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
