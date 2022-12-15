d5d6c04a42c6f58bd92e7f8a63104b3ad384e050
zekariyas7 <zekariyas7721@gmail.com>
2022-12-15 02:24:34 -0800

    0 task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_IN
+#define MAIN_IN
+
+#include <stdio.h>
+
+void positive_or_negative(int i):
+int largest_number(int a, int b, int c):
+void print_remaining_days(int moth, int day, int year):
+int convert_days(int month, int day):
+void print_times_table(int n):
+
+#endif	
